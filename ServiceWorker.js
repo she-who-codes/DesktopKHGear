3261e0cb2cbe86c5eb6749d62ff65e4e7bcb9cf9
Mandy Lowry <mandylowry@gmail.com>
2024-09-16 11:00:11 -0500

    first test build

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Black Hive Media-KH Gear App-.80";
+const contentToCache = [
+    "Build/KyleHockeyDesktop.loader.js",
+    "Build/KyleHockeyDesktop.framework.js",
+    "Build/KyleHockeyDesktop.data",
+    "Build/KyleHockeyDesktop.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
